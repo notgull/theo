4311baaf22915043536429ebe79cef3dd88e4149
John Nunley <dev@notgull.net>
2023-08-04 17:29:14 -0700

    Add the DCO
    Signed-off-by: John Nunley <dev@notgull.net>

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -14,3 +14,13 @@ We welcome contributions to this project. Please feel free to open GitHub issues
 All changes submitted to this repository are run through GitHub Actions and the workflow defined into the [`ci.yml`] file. If your change does not pass the tests described, it is unlikely to be merged.
 
 [`ci.yml`]: https://github.com/notgull/theo/blob/main/.github/workflows/ci.yml
+
+## DCO
+
+As an alternative to a Contributor License Agreement, this project uses a [Developer Certificate of Origin (DCO)](./DCO.txt) to ensure that contributors own the copyright terms of their contributions. In order to assert that you agree to the terms of the DCO, you must add the following line to every commit:
+
+```
+Signed-off-by: Your Name <email>
+```
+
+This can be done automatically by appending the `-s` option to `git commit`.
--- examples/winit.rs
+++ examples/winit.rs
@@ -1,23 +1,20 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later OR MPL-2.0
-// This file is a part of `piet-hardware`.
+// This file is a part of `theo`.
 //
-// `piet-hardware` is free software: you can redistribute it and/or modify it under the
+// `theo` is free software: you can redistribute it and/or modify it under the
 // terms of either:
 //
 // * GNU Lesser General Public License as published by the Free Software Foundation, either
 //   version 3 of the License, or (at your option) any later version.
 // * Mozilla Public License as published by the Mozilla Foundation, version 2.
-// * The Patron License (https://github.com/notgull/piet-hardware/blob/main/LICENSE-PATRON.md)
-//   for sponsors and contributors, who can ignore the copyleft provisions of the above licenses
-//   for this project.
 //
-// `piet-hardware` is distributed in the hope that it will be useful, but WITHOUT ANY
+// `theo` is distributed in the hope that it will be useful, but WITHOUT ANY
 // WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
 // PURPOSE. See the GNU Lesser General Public License or the Mozilla Public License for more
 // details.
 //
 // You should have received a copy of the GNU Lesser General Public License and the Mozilla
-// Public License along with `piet-hardware`. If not, see <https://www.gnu.org/licenses/>.
+// Public License along with `theo`. If not, see <https://www.gnu.org/licenses/>.
 
 //! Basic usage of `theo`, using `winit` as the windowing system.
 
